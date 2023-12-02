207aab51975ba41a2635923ddc8a4cdcd4ffad9e
Puhalenthi Ramesh Vidhya <57294477+Puhalenthi@users.noreply.github.com>
2023-12-02 12:03:59 -0500

    day2: use endswith instead of slicing

--- day2/advent_p1.py
+++ day2/advent_p1.py
@@ -11,13 +11,13 @@ with open("input.txt", "r") as f:
         values = game.split(" ")
         bad = False
         for i in range(2, len(values)-1, 2):
-            if values[i+1][0:-1] == "red":
+            if values[i+1].endswith("red"):
                 bad = True if (int(values[i]) > MAX_RED) else False
                 break
-            elif values[i+1][0:-1] == "green":
+            elif values[i+1].endswith("green"):
                 bad = True if (int(values[i]) > MAX_GREEN) else False
                 break
-            elif values[i+1][0:-1] == "blue":
+            elif values[i+1].endswith("blue"):
                 bad = True if (int(values[i]) > MAX_BLUE) else False
                 break
         if not bad:
--- day2/advent_p2.py
+++ day2/advent_p2.py
@@ -9,11 +9,11 @@ with open("input.txt", "r") as f:
         maxGreen = 0
         maxBlue = 0
         for i in range(2, len(values)-1, 2):
-            if values[i+1][0:-1] == "red" or values[i+1] == "red":
+            if values[i+1].endswith("red") or values[i+1] == "red":
                 maxRed = int(values[i]) if int(values[i]) > maxRed else maxRed
-            elif values[i+1][0:-1] == "green" or values[i+1] == "green":
+            elif values[i+1].endswith("green") or values[i+1] == "green":
                 maxGreen = int(values[i]) if int(values[i]) > maxGreen else maxGreen
-            elif values[i+1][0:-1] == "blue" or values[i+1] == "blue":
+            elif values[i+1].endswith("blue") or values[i+1] == "blue":
                 maxBlue = int(values[i]) if int(values[i]) > maxBlue else maxBlue
         total += maxRed * maxGreen * maxBlue
     
